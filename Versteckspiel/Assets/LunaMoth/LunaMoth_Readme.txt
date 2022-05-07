39aa63ee5cc190d83d6796d33acd555a6d653856
SolennHoffmann <soho1002@stud.hs-kl.de>
2022-05-07 13:24:45 +0200

    Luna Moth

---
+++ Versteckspiel/Assets/LunaMoth/LunaMoth_Readme.txt
@@ -0,0 +1,54 @@
+Luna Moth 3D model.
+
+Include Sample Scene in URP
+13 Animations in Root Motion
+Added extra folder with in-place animations
+4 Textures (4096x4096)
+3 Materials -URP support-
+1 Shader -URP support-from Two Sided Shaders +URP package 
+(for technical support or buy complete package put in contact with the publisher )
+1 Prefab
+1 Animator Controller
+1 Skybox
+2 Scripts
+1 Sample Scene in Universal Render Pipeline (URP)
+
+Low poly model
+
+2748 quad polygons on this mesh
+2876 vertices on this mesh
+
+Textures
+T_LunaMoth_D
+T_LunaMoth_N
+T_LunaMothWings_D
+T_LunaMothWings_N
+
+Animations List
+backward (684-712)
+fly (182-194)
+flyleft (225-237)
+flyrigh (239-251)
+idle (0-150)
+idle2 (363-513)
+landing (196-223)
+landing2 (545-572)
+takeoff (153-180)
+takeoff2 (516-543)
+turnleft (285-321)
+turnright (324-360)
+walk (254-282)
+
+Scene Demo
+keys for enable animations
+
+W = walk/fly
+S = backward
+A = flyleft/turnleft
+D = flyright/turnright
+Space = takeoff/landing/takeoff2/landing2
+------------
+Alpha1 = landing1
+Alpha2 = landing2
+
+enjoy game age:)
---
+++ Versteckspiel/Assets/LunaMoth/Scripts/Lunamoth.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lunamoth : MonoBehaviour
+{
+    private Animator lunamoth;
+    private bool State1 = true;
+    private bool State2 = false;
+
+    void Start()
+    {
+        lunamoth = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            State1 =! State1;
+            State2 = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            State2 =! State2;
+            State1 = false;
+        }
+        if (lunamoth.GetCurrentAnimatorStateInfo(0).IsName("idle")&&(State1==true))
+        {
+            lunamoth.SetBool("takeoff", false);
+            lunamoth.SetBool("landing", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)&&(State1==true))
+        {
+            lunamoth.SetBool("idle", false);
+            lunamoth.SetBool("takeoff", true);
+            lunamoth.SetBool("landing", true);
+            lunamoth.SetBool("fly", false);
+            lunamoth.SetBool("flyleft", false);
+            lunamoth.SetBool("flyright", false);
+        }
+        if (lunamoth.GetCurrentAnimatorStateInfo(0).IsName("idle2") && (State2 == true))
+        {
+            lunamoth.SetBool("takeoff2", false);
+            lunamoth.SetBool("landing2", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && (State2 == true))
+        {
+            lunamoth.SetBool("idle2", false);
+            lunamoth.SetBool("takeoff2", true);
+            lunamoth.SetBool("landing2", true);
+            lunamoth.SetBool("fly", false);
+            lunamoth.SetBool("flyleft", false);
+            lunamoth.SetBool("flyright", false);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            lunamoth.SetBool("flyleft", true);
+            lunamoth.SetBool("fly", false);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            lunamoth.SetBool("flyright", true);
+            lunamoth.SetBool("fly", false);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            lunamoth.SetBool("fly", true);
+            lunamoth.SetBool("flyleft", false);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            lunamoth.SetBool("fly", true);
+            lunamoth.SetBool("flyright", false);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            lunamoth.SetBool("walk", true);
+            lunamoth.SetBool("idle", false);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            lunamoth.SetBool("idle", true);
+            lunamoth.SetBool("walk", false);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            lunamoth.SetBool("backward", true);
+            lunamoth.SetBool("idle", false);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            lunamoth.SetBool("backward", false);
+            lunamoth.SetBool("idle", true);
+        }
+        if (Input.GetKeyDown(KeyCode.A)&&(State1==true))
+        {
+            lunamoth.SetBool("turnleft", true);
+            lunamoth.SetBool("idle", false);
+        }
+        if (Input.GetKeyUp(KeyCode.A)&&(State1==true))
+        {
+            lunamoth.SetBool("idle", true);
+            lunamoth.SetBool("turnleft", false);
+        }
+        if (Input.GetKeyDown(KeyCode.D)&&(State1==true))
+        {
+            lunamoth.SetBool("idle", false);
+            lunamoth.SetBool("turnright", true);
+        }
+        if (Input.GetKeyUp(KeyCode.D)&&(State1==true))
+        {
+            lunamoth.SetBool("idle", true);
+            lunamoth.SetBool("turnright", false);
+        }
+        if (Input.GetKeyDown(KeyCode.A) && (State2 == true))
+        {
+            lunamoth.SetBool("turnleft2", true);
+            lunamoth.SetBool("idle2", false);
+        }
+        if (Input.GetKeyUp(KeyCode.A) && (State2 == true))
+        {
+            lunamoth.SetBool("idle2", true);
+            lunamoth.SetBool("turnleft2", false);
+        }
+        if (Input.GetKeyDown(KeyCode.D) && (State2 == true))
+        {
+            lunamoth.SetBool("idle2", false);
+            lunamoth.SetBool("turnright2", true);
+        }
+        if (Input.GetKeyUp(KeyCode.D) && (State2 == true))
+        {
+            lunamoth.SetBool("idle2", true);
+            lunamoth.SetBool("turnright2", false);
+        }
+    }
+}
