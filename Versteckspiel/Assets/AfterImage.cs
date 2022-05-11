4ab3252118071f9277b1e222e7470f6c6863f982
Chiara <67367467+chba1002@users.noreply.github.com>
2022-05-11 12:51:38 +0200

    PlayerController_alpha

---
+++ Versteckspiel/Assets/AfterImage.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AfterImage : MonoBehaviour
+{
+    public Animator myAnimator;
+    public Renderer myRenderer;
+
+    public Animator targetAnimator;
+    public GameObject targetObject;
+
+    public float time;
+    public float intensity;
+    public float pow;
+    public float timeMax = 45;
+
+    public bool active;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > 0)
+        {
+            time--;
+            active = true;
+            intensity = (time / timeMax) * 10 * pow;
+            UpdateRenderer();
+        }
+        else
+        {
+            active = false;
+            intensity = 0;
+        }
+    }
+
+    void UpdateRenderer()
+    {
+        myRenderer.material.SetFloat("_Intensity", intensity);
+        myRenderer.material.SetFloat("_MKGlowPower", intensity);
+    }
+    public void Activate()
+    {
+        active = true;
+        transform.position = targetObject.transform.position;
+        transform.localScale = targetObject.transform.lossyScale;
+        transform.rotation = targetObject.transform.rotation;
+        myAnimator.Play(targetAnimator.GetCurrentAnimatorStateInfo(0).shortNameHash, 0, targetAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+
+        foreach (AnimatorControllerParameter param in targetAnimator.parameters)
+        {
+            if (param.type == AnimatorControllerParameterType.Float)
+            {
+                myAnimator.SetFloat(param.name, targetAnimator.GetFloat(param.name));
+            }
+
+            if (param.type == AnimatorControllerParameterType.Int)
+            {
+                myAnimator.SetInteger(param.name, targetAnimator.GetInteger(param.name));
+            }
+        }
+
+        myAnimator.speed = 0;
+        time = timeMax + 1;
+        Update();
+    }
+}
---
+++ Versteckspiel/Assets/AfterImagePool.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AfterImagePool : MonoBehaviour
+{
+    public PlayerMovement myPlayerMovementScript;
+    public GameObject prefab;
+    public int poolSize = 10;
+    public List<AfterImage> afterImages;
+
+    public int interval = 10;
+
+    public int time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myPlayerMovementScript = transform.root.GetComponent<PlayerMovement>();
+        Debug.Log("Start After Image Pool");
+        afterImages = new List<AfterImage>(poolSize);
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject nextAfterImage = Instantiate(prefab);
+            afterImages.Add(nextAfterImage.GetComponent<AfterImage>());
+            nextAfterImage.GetComponent<AfterImage>().targetObject = myPlayerMovementScript.Moth;
+            nextAfterImage.GetComponent<AfterImage>().targetAnimator = myPlayerMovementScript.animator;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time++;
+        if (time > interval)
+        {
+            time = 0;
+            AddAfterImage();
+        }
+    }
+
+    void AddAfterImage()
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            if (!afterImages[i].active)
+            {
+                afterImages[i].Activate(); break;
+            }
+        }
+    }
+}
