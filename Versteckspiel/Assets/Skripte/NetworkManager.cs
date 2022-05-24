b55c779ae2fb560246d4dff35faf94ddf32c07b0
Chiara <67367467+chba1002@users.noreply.github.com>
2022-05-24 18:39:26 +0200

    Photon Scripts

---
+++ Versteckspiel/Assets/Skripte/NetworkManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectToServer();
+    }
+
+    void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Try Connect To Server...");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected To Server.");
+        base.OnConnectedToMaster();
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 10;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+        PhotonNetwork.JoinOrCreateRoom("Room 1", roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a Room");
+        base.OnJoinedRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+    {
+        Debug.Log("New Player Joined The Room");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+}
