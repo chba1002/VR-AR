cae58d5d9f6f7ff2743c4b9a26e81feb3f609f40
Chiara <67367467+chba1002@users.noreply.github.com>
2022-07-13 16:39:46 +0200

    Dissolve Shader new

--- Versteckspiel/Assets/EnemyController.cs
+++ Versteckspiel/Assets/EnemyController.cs
@@ -2,64 +2,29 @@ using UnityEngine;
 using UnityEngine.VFX;
 using UnityEngine.Experimental.VFX;
 
-public enum EnemyType {  SmallEnemy, BigEnemy }
-
 public class EnemyController : MonoBehaviour
 {
-    Animator Animator;
     public bool aufloesenAktiv;
     public float verbleibendeAufloesedauerInSekunden = 0;
-    public bool angriffAktiv;
-    //public bool StrikeLightning = false;
-    public float verbleibendeAngriffsdauerInSekunden = 0;
-    public bool hatBereitsAngegriffen;
-    private schachfigur schachfigur;
-    public int AnzahlDerAngriffe;
 
-    public EnemyType EnemyType = EnemyType.SmallEnemy;
-
-    public GameObject spiderMesh;
+    public GameObject mothMesh;
     public Material dissolveShaderMaterial;
-    public VisualEffect Blitz;
-    //private Material dissolverShader;
     private Material material;
-    private SoundController soundController;
 
     // Start is called before the first frame update
     void Start()
     {
-        Blitz.enabled = false;
-        AnzahlDerAngriffe = 0;
-        angriffAktiv = false;
-        Animator = GetComponent<Animator>();
-
-        var schachfiguren = FindObjectsOfType<schachfigur>();
-
-        schachfigur = schachfiguren[0];
-
         // Erstelle neues Material auf basis des Prefab Materials, damit jeder Enemy eigenes Material hat.
         var dissolverShader = new Material(dissolveShaderMaterial);
 
         // Weise neu erstelltes Material dem Spinnen Mesh zu.
-        spiderMesh.GetComponent<Renderer>().sharedMaterial = dissolverShader;
+        mothMesh.GetComponent<Renderer>().sharedMaterial = dissolverShader;
 
         // Speichere das Material in der variable 'material' zwischen.
-        material = spiderMesh.GetComponent<Renderer>().sharedMaterial;
-        soundController = FindObjectsOfType<SoundController>()[0];
+        material = mothMesh.GetComponent<Renderer>().sharedMaterial;
     }
     void Update()
     {
-        if (angriffAktiv)
-        {
-            verbleibendeAngriffsdauerInSekunden -= Time.deltaTime;
-
-            if (verbleibendeAngriffsdauerInSekunden <= 0)
-            {
-                angriffAktiv = false;
-                hatBereitsAngegriffen = true;
-            }
-        }
-
         if (aufloesenAktiv)
         {
             verbleibendeAufloesedauerInSekunden -= Time.deltaTime;
@@ -75,64 +40,9 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    public bool EnemyIsDead()
-    {
-        return Animator.GetCurrentAnimatorStateInfo(0).IsName("dead");
-    }
-
-    public void Attack()
-    {
-        angriffAktiv = true;
-        verbleibendeAngriffsdauerInSekunden = 2;
-        Animator.SetTrigger("AttackTrigger");
-
-        AnzahlDerAngriffe = AnzahlDerAngriffe + 1;
-
-        switch (EnemyType)
-        {
-            case EnemyType.SmallEnemy:
-                soundController.PlaySpinneGreiftAn();
-                break;
-            case EnemyType.BigEnemy:
-                soundController.PlayBigEnemyAttackSound();
-                break;
-        }
-    }
-
     public void Aufloesen()
     {
-        // Blitz auslösen
-        Blitz.enabled = true;
-        Blitz.Play();
-        // Kristall wird abgezogen
-        Energy.RemoveEnergy();
         aufloesenAktiv = true;
         verbleibendeAufloesedauerInSekunden = 2;
-
-
-        switch (EnemyType)
-        {
-            case EnemyType.SmallEnemy:
-                soundController.PlaySpinneBesiegt();
-                break;
-            case EnemyType.BigEnemy:
-                soundController.PlayBigEnemyBesiegtSound();
-                break;
-        }
-
-    }
-
-    /// <summary>
-    /// Wird in Animationsfenster aufgerufen, wenn Gegner Spieler berührt.
-    /// </summary>
-    public void ZeitpunktBeruehrungSpieler()
-    {
-        schachfigur.ReceiveDamage();
-        // Blitzattacke der Spinne schlägt ein
-    }
-
-    public void ReceiveDamage()
-    {
-        Animator.SetTrigger("ReceiveDamageTrigger");
     }
 }
--- Versteckspiel/Assets/ShootAtMoth.cs
+++ Versteckspiel/Assets/ShootAtMoth.cs
@@ -14,7 +14,39 @@ public class ShootAtMoth : MonoBehaviour
     public TMP_Text TimePlayed;
     public Timer mytimerscript;
 
-    public void ShootMoth()
+    public bool aufloesenAktiv;
+    public float verbleibendeAufloesedauerInSekunden = 0;
+
+    public GameObject mothMesh1;
+    public GameObject mothMesh2;
+    public GameObject mothMesh3;
+    public GameObject mothMesh4;
+    public GameObject mothMesh5;
+    public Material dissolveShaderMaterial;
+    private Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Erstelle neues Material auf basis des Prefab Materials, damit jeder Enemy eigenes Material hat.
+        var dissolverShader = new Material(dissolveShaderMaterial);
+
+        // Weise neu erstelltes Material dem Spinnen Mesh zu.
+        mothMesh1.GetComponent<Renderer>().sharedMaterial = dissolverShader;
+        mothMesh2.GetComponent<Renderer>().sharedMaterial = dissolverShader;
+        mothMesh3.GetComponent<Renderer>().sharedMaterial = dissolverShader;
+        mothMesh4.GetComponent<Renderer>().sharedMaterial = dissolverShader;
+        mothMesh5.GetComponent<Renderer>().sharedMaterial = dissolverShader;
+
+        // Speichere das Material in der variable 'material' zwischen.
+        material = mothMesh1.GetComponent<Renderer>().sharedMaterial;
+        material = mothMesh2.GetComponent<Renderer>().sharedMaterial;
+        material = mothMesh3.GetComponent<Renderer>().sharedMaterial;
+        material = mothMesh4.GetComponent<Renderer>().sharedMaterial;
+        material = mothMesh5.GetComponent<Renderer>().sharedMaterial;
+    }
+
+        public void ShootMoth()
     {
         //Sobald der Strahl Motte berührt, kann man schießen. Check
         //Beide Strahlen müssen Motte treffen, damit sie stirbt. Sonst passiert nichts.
@@ -23,7 +55,15 @@ public class ShootAtMoth : MonoBehaviour
         //Motten gewinnen, sobald Zeit abläuft, FM gewinnt, sobald innerhalb der Zeit alle Motten gefroffen wurden, dann jeweiliges Menü anzeigen
 
         print("Motte getroffen!");
+
         // Hier Dissolve Shader einfügen @Chi
+        aufloesenAktiv = true;
+        verbleibendeAufloesedauerInSekunden -= Time.deltaTime;
+        var dissolverValue = 1 - verbleibendeAufloesedauerInSekunden / 2;
+        //Debug.Log("DissolveValue: " + dissolverValue + "(" + verbleibendeAufloesedauerInSekunden + "");
+
+        material.SetFloat("_Dissolve", dissolverValue);
+
         Destroy(this.gameObject); //oder deaktivieren
     }
 
@@ -48,5 +88,11 @@ public class ShootAtMoth : MonoBehaviour
 
         string StartTime = mytimerscript.timeValue.ToString("f0");
         TimePlayed.text = StartTime;
+    }
+
+    public void Aufloesen()
+    {
+        aufloesenAktiv = true;
+        verbleibendeAufloesedauerInSekunden = 2;
     }
 }
