1a587df0099f3c4443cce6e4bf88cf06f7445d82
Bonnie <81580119+bonna12@users.noreply.github.com>
2022-07-27 21:40:45 +0200

    Fledermaus und Motten sind nun beweglich.

--- Versteckspiel/Assets/Scripts/Game/Network/NetworkPlayer.cs
+++ Versteckspiel/Assets/Scripts/Game/Network/NetworkPlayer.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.XR.Interaction.Toolkit;
+using Unity.XR.CoreUtils;
+using UnityEngine.XR;
 
 public class NetworkPlayer : MonoBehaviour
 {
     public Transform Body;
+    public Transform BodyRig_CenterEyeAnchor;
     private PhotonView photonView;
 
     // private Transform BodyRig;
@@ -12,23 +16,29 @@ public class NetworkPlayer : MonoBehaviour
     void Start()
     {
         photonView = GetComponent<PhotonView>();
-        // XRRig rig = FindObjectOfType<XRRig>();
-        //  Body = rig.transform.Find("OVRCameraRig/TrackingSpace/CenterEyeAnchor");
+        XROrigin rig = FindObjectOfType<XROrigin>();
+       // BodyRig = rig.transform.Find("OVRCameraRig/TrackingSpace/CenterEyeAnchor");
 
         Debug.Log($"NetworkPlayerScript is executed. Player is mine: {photonView.IsMine}");
 
         if (photonView.IsMine)
         {
             Body.gameObject.SetActive(false);
-            //     MapPosition(Body, BodyRig);
         }
     }
-/*
+
+
+    private void Update()
+    {
+        if (photonView.IsMine)
+        {
+            MapPosition(Body, BodyRig_CenterEyeAnchor);
+        }
+    }
 
     void MapPosition(Transform target, Transform rigTransform)
     {
         target.position = rigTransform.position;
         target.rotation = rigTransform.rotation;
     }
-*/
 }
